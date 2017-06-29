6fc612a79f4318a79bea331a1259c4cf50ea55b0
Alexandra Levy <alexandrasophielevy@gmail.com>
2017-06-29 14:28:43 +0000

    refactor with forEach

---
+++ waldo/waldoRefactor.js
@@ -0,0 +1,13 @@
+function findWaldo(arr, found) {
+  arr.forEach(function(arr) {
+    if(arr === "Waldo") {
+      found();
+    }
+  })
+}
+
+function actionWhenFound() {
+ console.log("Found him!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
